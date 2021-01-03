bf524eb9c30cc9b92c5f8e8e87919864543fbcf9
hossamkoky.98@gmail.com <hossamkoky.98@gmail.com>
2021-01-03 16:47:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lecture_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] ID = new int[30];
+             for (int i = 0; i < ID.Length; i++)
+             {
+                 ID[i] = Convert.ToInt32(Console.ReadLine());
+             }
+             double max;
+             double[] score = new double[30];
+            for (int i = 0; i < score.Length; i++)
+            {
+                score[i] = Convert.ToInt32(Console.ReadLine());
+                if (score[i] < 50)
+                {
+                    score[i] += score[i] + 2;
+                }
+                for (int j = 0; j < score.Length; j++)
+                {
+
+                }
+
+
+
+            }
+        }
+    }
+}
